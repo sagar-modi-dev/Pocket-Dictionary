3d83a0a1c3c14153b95a973bdbaa3f40c7ccc3b3
sm185435 <sm185435@ncr.com>
2025-12-02 18:58:20 +0530

    Bump version to 1.0.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "com.sagar.pocketdictionary"
         minSdk = 24
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.0.1"
 
         testInstrumentationRunner = "com.sagar.pocketdictionary.HiltTestRunner"
     }
